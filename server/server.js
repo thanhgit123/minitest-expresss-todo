bb0dcd3cbf13d02221106c51a4f0cf0faea49576
thanhgit123 <hai880345@gmail.com>
2023-12-11 16:05:13 +0700

    add

---
+++ server/server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+const PORT = 7000;
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+const readDb = fs.readFileSync('db.json');
+const data = JSON.parse(readDb);
+
+app.get('/api/v1/todos',async(req,res)=>{
+    const {per_page} = req.query;
+    const arrSlice = data.todos.slice(0, +per_page)
+    res.status(200).json(arrSlice);
+});
+
+// Add new Todo with POST
+app.post('/api/v1/todos',(req,res)=>{
+    data.todos.unshift(req.body);
+    fs.writeFileSync('db.json',JSON.stringify(data));
+    res.status(201).json({
+        message : "Add sucess",
+        mama : data.todos
+    })
+});
+
+// changecompleted with patch
+app.patch('/api/v1/todos/:id',(req,res)=>{
+    const index = data.todos.findIndex((item)=>item.id == req.params.id);
+    data.todos[index].completed = !req.body.completed;
+    fs.writeFileSync('db.json',JSON.stringify(data));
+    res.status(200).json({
+        mama : data.todos
+    })
+})
+// update with put method
+    app.put('/api/v1/todos/:id',(req,res)=>{
+    const index = data.todos.findIndex((some)=>some.id == req.params.id);
+    data.todos[index] = req.body
+    fs.writeFileSync('db.json',JSON.stringify(data));
+    res.status(200).json({
+        message : "update  sucess",
+        mama : data.todos
+    })
+});
+
+//delete with delete method
+app.delete('/api/v1/todos/:id',(req,res)=>{
+    const newArr = data.todos.filter((item)=>item.id != req.params.id);
+    data.todos = newArr;
+    fs.writeFileSync('db.json',JSON.stringify(data));
+    res.status(200).json({
+        message  : "Del sucess",
+        mama : data.todos
+    })
+})
+// clear all
+app.delete('/api/v1/todos',(req,res)=>{
+    data.todos = [];
+    fs.writeFileSync('db.json',JSON.stringify(data));
+    res.status(200).json({
+        mama:data.todos
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log(` Server is running to http://localhost:${PORT}`)          
+})
---
+++ src/components/todo/Todo.jsx
@@ -0,0 +1,164 @@
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+
+export default function Todo() {
+  const [newTodo, setNewTodo] = useState({name: ""});
+  const [allToDo, setAllTodo] = useState([]);
+  const [edit, setEdit] = useState(false);
+  const [loading,setLoading] = useState(false);
+  const inputRef = useRef(null);
+  const [flag,setFlag] = useState(false);
+
+  // get data form server to render
+  const handleGetData = async () => {
+    setLoading(true);
+    const taskPerPage = 4;
+    try {
+      const res = await axios.get(`http://localhost:7000/api/v1/todos?per_page=${taskPerPage}`);
+      setAllTodo(res.data);
+    } catch (error) {
+      console.log(error);
+    }finally{
+      setTimeout(() => {
+        setLoading(false)
+      }, 800);
+    }
+  };
+  useEffect(() => {
+    handleGetData();
+  }, [flag]);
+
+  // Add newtodo
+  const handleAddToDo = async () => {
+    try {
+      const res = await axios.post("http://localhost:7000/api/v1/todos", {
+        ...newTodo,
+        id: Math.floor(Math.random() * 9999999999),
+        completed : false
+      });
+      setFlag(!flag)
+      setNewTodo({ name: ""});
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+// change completed to line-through
+  const handleChangeCompleted = async (item)=>{
+      try {
+        const res = await axios.patch(`http://localhost:7000/api/v1/todos/${item.id}`,item);
+        setFlag(!flag)
+      } catch (error) {
+        console.log(error);
+      }
+  }
+  //update todos with put
+  const handleEdit = async (item) => {
+    setEdit(true);
+    setNewTodo(item);
+  };
+  const handleSave = async () => {
+    try {
+      const res = await axios.put(
+        `http://localhost:7000/api/v1/todos/${newTodo.id}`,
+        newTodo
+      );
+      setFlag(!flag)
+      setNewTodo({name:""})
+      setEdit(false)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //Del with delete method
+  const handleDel = async (id) => {
+    try {
+      const accept = window.confirm("Are you sure ?")
+      if(accept){
+        const res = await axios.delete(
+          `http://localhost:7000/api/v1/todos/${id}`
+        );
+        setFlag(!flag)
+        setNewTodo({...newTodo,name : ''})
+      }else{
+        return;
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //del clear all
+  const handleClearAll = async()=>{
+    try {
+      const accept = window.confirm("Are you sure ?")
+      if(accept){
+        const res = await axios.delete('http://localhost:7000/api/v1/todos')
+        setFlag(!flag)
+      }else{
+        return;
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <div className="bg-gradient-to-r from-cyan-300 to-blue-500 h-[100vh] pt-4 pb-[70px]">
+        <div className="bg-gradient-to-r from-emerald-500 via-purple-600 to-pink-500  pb-6 h-[500px] w-[500px] m-auto mt-4 pt-5  leading-10">
+          <h1 className="text-3xl text-center ">Todo App</h1>
+          <br />
+          <input
+            type="text"
+            ref={inputRef}
+            placeholder="Add task"
+            value={newTodo.name}
+            name="name"
+            onChange={(e) => setNewTodo({...newTodo,[e.target.name]:e.target.value})}
+            className="w-[370px] pl-4 ml-5 rounded-[3px]"
+          />
+          <button
+            className="bg-green-400 w-[50px] ml-4 rounded-[4px]"
+            onClick={edit ? handleSave : handleAddToDo}
+          >
+            {edit ? <span>Save</span> : <span>Add</span>}
+          </button>
+          {allToDo.map((item, index) => {
+            return (
+              <div key={index} className="pt-5 leading-10 flex">
+                <div className="bg-white w-[370px] ml-5 h-[40px] rounded-[3px] pl-5 cursor-pointer" onClick={()=>handleChangeCompleted(item)}>
+                  {" "}
+                 <p className={item.completed ? "line-through" : ""}> {item.name}</p> 
+                </div>
+                <div className=" ml-4 flex justify-between ">
+                  <button
+                    className="bg-sky-200 w-[40px] rounded-[3px]"
+                    onClick={() => handleDel(item.id)}
+                  >
+                    Del
+                  </button>
+                  <button
+                    className="bg-green-400 w-[40px] ml-2 rounded-[3px]"
+                    onClick={() => handleEdit(item)}
+                  >
+                    Edit
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+
+          <div className=" pt-7 leading-10 flex">
+            <div className="bg-white w-[370px] ml-5 h-[40px] rounded-[3px] pl-5">
+              You have <span className="text-red-400">{allToDo.length}</span>{" "}
+              pending task !
+            </div>
+            <button className="bg-sky-200 w-[60px] rounded-[3px] ml-4 " onClick={handleClearAll}>
+              Clear All
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
